8be43ec6e2b4d53764e37bdfddd749b652adb429
Karthik Nandam <carthicknandam@gmail.com>
2026-02-01 23:18:38 +0530

    fix: bug fix and refractor for test header

--- app/api/pastes/[id]/route.ts
+++ app/api/pastes/[id]/route.ts
@@ -1,13 +1,13 @@
-import { redis } from "@/app/utils/redis";
+import { CacheValue, redis } from "@/app/utils/redis";
 import { NextRequest, NextResponse } from "next/server";
+import { getNowMs } from "@/app/utils/helper";
 
 export async function GET(
-  _req: NextRequest,
+  req: NextRequest,
   { params }: { params: Promise<{ id: string }> },
 ) {
   try {
-    let param = await params;
-    let id = param.id;
+    const { id } = await params;
 
     if (!id) {
       return NextResponse.json(
@@ -16,37 +16,63 @@ export async function GET(
       );
     }
 
-    let data = await redis.get(id);
+    // fetch data
+    const data = (await redis.get(id)) as CacheValue | null;
 
-    if (!data) {
+    if (!data || !data.value) {
       return NextResponse.json(
-        {
-          success: false,
-          message: "Data not found",
-        },
+        { success: false, message: "Data not found" },
         { status: 404 },
       );
     }
 
-    let res = {
-      content: data.value,
-      remaining_views: data?.max_views || null,
-      expires_at: data?.ttl,
-    };
+    // TTL check with header here as per tests
+    if (data.expires_at) {
+      const now = getNowMs(req);
+      const expiresAt = Number(data.expires_at);
+
+      if (!Number.isNaN(expiresAt) && now >= expiresAt) {
+        await redis.del(id); // cleanup expired key
+        return NextResponse.json(
+          { success: false, message: "Expired" },
+          { status: 404 },
+        );
+      }
+    }
+
+    // views handling
+    let remainingViews: number | null = null;
+
+    if (data.max_views) {
+      const remaining = await redis.update(id);
+      remainingViews = remaining;
+
+      if (remaining < 0) {
+        await redis.del(id);
+        return NextResponse.json(
+          { success: false, message: "Data not found" },
+          { status: 404 },
+        );
+      }
+    }
 
     return NextResponse.json(
       {
         success: true,
-        data: res,
+        data: {
+          content: data.value,
+          remaining_views: remainingViews,
+          expires_at: data.expires_at
+            ? new Date(Number(data.expires_at)).toISOString()
+            : null,
+        },
       },
       { status: 200 },
     );
-  } catch (error) {
+  } catch (err) {
+    console.error("GET error:", err);
     return NextResponse.json(
-      {
-        success: false,
-        error: "Internal Server Error",
-      },
+      { success: false, error: "Internal Server Error" },
       { status: 500 },
     );
   }
